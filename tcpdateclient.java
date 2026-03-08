48d06ddc81047728f43ed4c6202fe3a9789a7a93
Yasar Arafath <144357170+yasararafath22@users.noreply.github.com>
2026-03-09 00:32:35 +0530

    Add TCP Date Client implementation

---
+++ tcpdateclient.java
@@ -0,0 +1,45 @@
+// TCP Date Client--tcpdateclient.java
+
+import java.net.*;
+import java.io.*;
+
+class tcpdateclient
+{
+    public static void main (String args[])
+    {
+        Socket soc;
+        BufferedReader dis;
+        String sdate;
+        PrintStream ps;
+
+        try
+        {
+            InetAddress ia = InetAddress.getLocalHost();
+
+            if (args.length == 0)
+                soc = new Socket(InetAddress.getLocalHost(), 4444);
+            else
+                soc = new Socket(InetAddress.getByName(args[0]), 4444);
+
+            // Read date from server
+            dis = new BufferedReader(
+                    new InputStreamReader(soc.getInputStream()));
+            sdate = dis.readLine();
+
+            System.out.println(
+                    "The date/time on server is : " + sdate);
+
+            // Send client address to server
+            ps = new PrintStream(soc.getOutputStream());
+            ps.println(ia);
+
+            ps.close();
+            dis.close();
+            soc.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("THE EXCEPTION is : " + e);
+        }
+    }
+}
